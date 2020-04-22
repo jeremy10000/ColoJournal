7eaf0012572065a5b65ea1b202a02643fb309e53
Jérémy <jeremyjulienvisio@outlook.fr>
2020-04-22 16:02:12 +0200

    Journal/Page Models

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Journal(models.Model):
+
+    name = models.CharField(max_length=80, blank=False, null=False)
+    date = models.DateField(blank=False, null=False)
+    shared = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Journaux"
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+
+    name = models.CharField(max_length=80, blank=False, null=False)
+    text = models.TextField(blank=False, null=False)
+    journal = models.ForeignKey(Journal, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name

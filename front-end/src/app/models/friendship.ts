188f80c2aea2f7c830f9ccfd8b16ff6d9c95f2a7
Jérémy <jeremyjulienvisio@outlook.fr>
2020-04-26 10:38:36 +0200

    Models

---
+++ front-end/src/app/models/friendship.ts
@@ -0,0 +1,5 @@
+export interface FriendshipModel {
+    id: number;
+    mail: string;
+    status: string;
+}
---
+++ front-end/src/app/models/page.ts
@@ -0,0 +1,7 @@
+export interface PageModel {
+    id: number;
+    name: string;
+    text: string;
+    journal: number;
+    journal_name: string;
+}

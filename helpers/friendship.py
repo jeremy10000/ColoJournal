8e055467ceaa7aa90c96664488d03604daeefb0e
Jérémy <jeremyjulienvisio@outlook.fr>
2020-04-26 10:58:45 +0200

    new helpers functions

--- helpers/friendship.py
+++ helpers/friendship.py
@@ -18,20 +18,48 @@ def check_proposition(sender, receiver):
 def list_relationships(user):
     """ Lists friendship requests sent and received from the user. """
     data = {
+        # My friends
         'yes': [],
+        # Not friends.
         'no': [],
+        # Request sent - waiting for a response
         'waiting': [],
+        # Request received - Need Answer.
+        'received': [],
     }
     relations = Friendship.objects.filter(
         Q(sender=user) | Q(receiver=user)
     )
+    
     for relation in relations:
-        if relation.status == 'y':
-            data.get('yes').append(FriendshipSerializer(relation).data)
-        elif relation.status == 'n':
-            data.get('no').append(FriendshipSerializer(relation).data)
-        else:
-            data.get('waiting').append(FriendshipSerializer(relation).data)
+        # Requests received.
+        if relation.receiver == user:
+            change = FriendshipSerializer(relation).data
+            change['user_id'] = int(relation.sender.id)
+            change['mail'] = str(relation.sender)
+            change.pop('receiver')
+            change.pop('sender')
+            if relation.status == 'w':
+                data.get('received').append(change)
+            elif relation.status == 'n':
+                data.get('no').append(change)
+            else:
+                data.get('yes').append(change)
+
+        # Requests sent
+        if relation.sender == user:
+            change = FriendshipSerializer(relation).data
+            change['user_id'] = int(relation.receiver.id)
+            change['mail'] = str(relation.receiver)
+            change.pop('receiver')
+            change.pop('sender')
+
+            if relation.status == 'w':
+                data.get('waiting').append(change)
+            elif relation.status == 'n':
+                data.get('no').append(change)
+            else:
+                data.get('yes').append(change)
 
     return data
 
@@ -45,3 +73,16 @@ def check_user(request):
                                  email=request.data.get('receiver', None))
 
     return sender, receiver
+
+
+def check_friend(request_user, friend):
+    """ Check if he's a friend. """
+    try:
+        relation = Friendship.objects.get(
+            Q(sender=request_user, receiver=friend, status='y') | 
+            Q(sender=friend, receiver=request_user, status='y')
+        )
+        return True
+
+    except Friendship.DoesNotExist:
+        return False

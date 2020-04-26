5e6214f1aeb24ebd5263b9174809bb2ba0466f07
Jérémy <jeremyjulienvisio@outlook.fr>
2020-04-26 10:50:19 +0200

    new : PageListAPIView, JournalListAPIView

--- journal/api/views.py
+++ journal/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from helpers.journal import list_journals, list_pages
+from helpers.journal import list_journals, list_pages, check_shared_journals, check_shared_pages
+from helpers.friendship import check_friend
 from journal.models import Journal, Page
 from users.models import User
 
@@ -79,7 +80,8 @@ class PageViewSet(viewsets.ModelViewSet):
     def list(self, request):
         """ List the pages of a journal. """
 
-        journal = request.data.get('journal', None)
+        # journal = request.data.get('journal', None)
+        journal = request.query_params.get('journal', None)
         if journal is None:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
@@ -130,3 +132,49 @@ class PageViewSet(viewsets.ModelViewSet):
         self.perform_destroy(page)
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class JournalListAPIView(generics.ListAPIView):
+    """ List journals for friends """
+    queryset = Journal.objects.all()
+    serializer_class = JournalSerializer
+
+    def list(self, request):
+        user = request.query_params.get('id', None)
+        user = get_object_or_404(User.objects.all(), id=user)
+        # a friend ?
+        is_friend = check_friend(self.request.user, user)
+        if is_friend is False:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        shared_journals = check_shared_journals(user)
+
+        return Response(shared_journals, status.HTTP_200_OK)
+
+
+class PageListAPIView(generics.ListAPIView):
+    """ List journals for friends """
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
+
+    def list(self, request):
+        print('request.query_params', request.query_params)
+        journal = request.query_params.get('id', None)
+        journal = get_object_or_404(Journal.objects.all(), id=journal)
+        print('journal demandé', journal.name, 'id:', journal.id, 'user:', journal.user)
+        print('self.request.user:', self.request.user, 'id:', self.request.user.id)
+        # vérifie si l'user demandé est l'ami de celui qui demande...
+        is_friend = check_friend(self.request.user, journal.user)
+        # pas ami
+        if is_friend is False:
+            print("Pas ami, pas de pages !")
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        # c'est un ami, on regarde si l'user demandé à des journaux partagés
+        print("OUI")
+        shared_pages = check_shared_pages(journal.user, journal.id)
+        if shared_pages is False:
+            print("Journal pas partagé, pas de pages !")
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        
+        
+        return Response(shared_pages, status.HTTP_200_OK)
